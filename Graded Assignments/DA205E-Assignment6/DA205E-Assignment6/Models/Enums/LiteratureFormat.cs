f0afb66474f9f3bcaa52668fb35fa5c292160e08
Sixten Peterson <sixten.peterson@winterzone.se>
2026-05-26 13:53:46 +0200

    Created the enums

---
+++ Graded Assignments/DA205E-Assignment6/DA205E-Assignment6/Models/Enums/LiteratureFormat.cs
@@ -0,0 +1,13 @@
+﻿// Sixten Peterson (AQ9300) 2026-05-26
+
+namespace DA205E_Assignment6.Models.Enums
+{
+    /// <summary>
+    /// Enum representing the format of a literature, it may be physical or digital.
+    /// </summary>
+    public enum LiteratureFormat
+    {
+        Physical,
+        Digital
+    }
+}
---
+++ Graded Assignments/DA205E-Assignment6/DA205E-Assignment6/Models/Enums/LiteratureStatus.cs
@@ -0,0 +1,14 @@
+﻿// Sixten Peterson (AQ9300) 2026-05-26
+
+namespace DA205E_Assignment6.Models.Enums
+{
+    /// <summary>
+    /// Enum representing the status of a literature resoruce. Literature may be owned by the user, borrowed from a library or friend or lent out to a friend.
+    /// </summary>
+    public enum LiteratureStatus
+    {
+        Owned,
+        Borrowed,
+        Lent
+    }
+}
