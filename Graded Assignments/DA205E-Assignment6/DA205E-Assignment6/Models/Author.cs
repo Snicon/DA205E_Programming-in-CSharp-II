20f4ba4f7cdddd0b49abb2c8b449f9084c829ed9
Sixten Peterson <sixten.peterson@winterzone.se>
2026-05-28 03:32:11 +0200

    Added new author record, will be used for the citation strategies

---
+++ Graded Assignments/DA205E-Assignment6/DA205E-Assignment6/Models/Author.cs
@@ -0,0 +1,40 @@
+﻿namespace DA205E_Assignment6.Models
+{
+    public record Author
+    {
+        #region Fields
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string MiddleName
+        {
+            get => middleName;
+            set => middleName = value;
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+        #endregion
+
+        #region Constructor
+        public Author(string firstName, string middleName, string lastName) 
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+        #endregion
+    }
+}
