00a36db4c071185222d41ba867b2b5e0f51bdd4e
Sixten Peterson <sixten.peterson@winterzone.se>
2026-06-03 11:42:42 +0200

    Added migrations and updated to database, database does not contain any sensitive info and is provided for example data.

---
+++ Graded Assignments/DA205E-Assignment6/DA205E-Assignment6/Migrations/20260602162851_InitialCreate.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DA205E_Assignment6.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Literatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    YearPublished = table.Column<int>(type: "INTEGER", nullable: false),
+                    Format = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 21, nullable: false),
+                    ISBN = table.Column<string>(type: "TEXT", nullable: true),
+                    Edition = table.Column<int>(type: "INTEGER", nullable: true),
+                    Publisher = table.Column<string>(type: "TEXT", nullable: true),
+                    City = table.Column<string>(type: "TEXT", nullable: true),
+                    JournalName = table.Column<string>(type: "TEXT", nullable: true),
+                    Volume = table.Column<int>(type: "INTEGER", nullable: true),
+                    Issue = table.Column<int>(type: "INTEGER", nullable: true),
+                    Pages = table.Column<string>(type: "TEXT", nullable: true),
+                    URL = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Literatures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseLiterature",
+                columns: table => new
+                {
+                    CoursesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LiteratureId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseLiterature", x => new { x.CoursesId, x.LiteratureId });
+                    table.ForeignKey(
+                        name: "FK_CourseLiterature_Courses_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseLiterature_Literatures_LiteratureId",
+                        column: x => x.LiteratureId,
+                        principalTable: "Literatures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseLiterature_LiteratureId",
+                table: "CourseLiterature",
+                column: "LiteratureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseLiterature");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Literatures");
+        }
+    }
+}
